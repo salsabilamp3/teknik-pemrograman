237bb57dc8cc19feecb4dae5ae920c1bd74abd14
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-02-11 12:58:48 +0700

    adding soal operator 1

---
+++ Pertemuan2/src/pertemuan2/Operator1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author Tata
+ */
+public class Operator1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        
+        System.out.println("Sebelum dibulatkan: "+x);
+        System.out.println("Setelah dibulatkan: "+nx);
+    }
+    
+}
