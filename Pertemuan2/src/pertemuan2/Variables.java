31c2d01f8963005fd74a30add873d661ffc8a5e0
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-02-11 12:31:34 +0700

    creating soal variables

---
+++ Pertemuan2/src/pertemuan2/Variables.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author Tata
+ */
+public class Variables {
+
+    public static final double CM_PER_INCH = 2.54;
+    public static void main(String[] args) {
+        //final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+    }
+    
+}
