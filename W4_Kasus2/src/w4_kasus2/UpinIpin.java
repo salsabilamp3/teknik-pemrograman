0fda236c4f7d610586f9fc1dfe870e86769ae9d0
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-02-26 15:39:01 +0700

    adding main method

---
+++ W4_Kasus2/src/w4_kasus2/UpinIpin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w4_kasus2;
+
+/**
+ *
+ * <h1>Add Stok</h1>
+ * Kasus ini bertujuan untuk mempelajari tentang constructor.
+ * Dalam sebuah class, memungkinkan adanya lebih dari
+ * satu constructor, dan dalam class tersebut pun
+ * bisa memanggil constructor lainnya dengan this().
+ * 
+ * @author : Salsabila Maharani Putri
+ */
+
+public class UpinIpin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Item name = new Item("Upin");
+    }
+    
+}
