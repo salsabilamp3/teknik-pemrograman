238c7c8410f949e54b358a05a41da98b6b5ffcc5
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-02-26 15:40:22 +0700

    adding class KelasSatu

---
+++ W4_Kasus3/src/w4_kasus3/KelasSatu.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w4_kasus3;
+
+/**
+ *
+ * @author : Salsabila Maharani Putri
+ */
+public class KelasSatu {
+    {
+        /*Instance method*/
+        System.out.println(11);
+    }
+    static
+    {
+        /*Static method*/
+        System.out.println(2);
+    }
+    public KelasSatu(int i)
+    {
+        /*Constructor with 1 argument*/
+        System.out.println(3);
+    }
+    public KelasSatu()
+    {
+        /*Constructor with 0 argument*/
+        System.out.println(4);
+    }
+}
