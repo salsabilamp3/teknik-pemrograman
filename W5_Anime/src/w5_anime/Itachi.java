da470ff7de877084a3fe8e17fd325be139bbc964
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-03-11 12:02:00 +0700

    adding class Itachi

---
+++ W5_Anime/src/w5_anime/Itachi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w5_anime;
+
+/**
+ *
+ * @author Salsabila Maharani Putri
+ */
+public class Itachi extends Rikudo {
+    private String KekkeiGenkai = "Susanoo";
+    private String Dojutsu = super.Dojutsu;
+    
+    void printKekkeiGenkai() {
+        System.out.println(this.KekkeiGenkai);
+    }
+    
+    void printDojutsu() {
+        System.out.println(this.Dojutsu);
+        setDojutsu();
+        System.out.println(this.Dojutsu);
+    }
+    
+    private void setDojutsu() {
+        this.Dojutsu = "Mangekyou Sharingan";
+    }
+}
