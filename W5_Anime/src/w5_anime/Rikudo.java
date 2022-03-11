79a41627114299016d11f9f8e2bd2ef02db3a6bf
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-03-11 12:01:40 +0700

    adding class Rikudo

---
+++ W5_Anime/src/w5_anime/Rikudo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w5_anime;
+
+/**
+ *
+ * @author Salsabila Maharani Putri
+ */
+public class Rikudo {
+    protected String Dojutsu = "Rinnegan";
+    protected String Senjutsu = "Sage Mode";
+}
