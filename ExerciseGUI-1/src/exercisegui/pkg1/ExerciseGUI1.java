3e8d463d0b48e5e8c13b5390908681d164727c25
salsabilamp3 <95154453+salsabilamp3@users.noreply.github.com>
2022-05-16 10:47:58 +0700

    adding exercise GUI 1

---
+++ ExerciseGUI-1/src/exercisegui/pkg1/ExerciseGUI1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercisegui.pkg1;
+
+/**
+ *
+ * @author Tata
+ */
+public class ExerciseGUI1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frame1().setVisible(true);
+    }
+    
+}
